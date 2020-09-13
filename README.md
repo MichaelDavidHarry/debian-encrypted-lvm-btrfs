530b2b13e202167dc8768371c6c22455c75778a5
Michael Harry <michael@compilefailed.com>
2020-09-12 22:53:21 -0500

    Add installer script file and update readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # debian-encrypted-lvm-btrfs
 A script to use during the Debian Installer to create an encrypted Btrfs root with subvolumes and snapper snapshots on LVM.
+
+## To Use:
+
+1. During the Debian Installer process at the 'Load installer components from CD' step, select the 'network-console' component. Continue.
+2. Enter a remote installation password, and choose continue.
+3. Use SSH to connect to the installer.
+4. Choose 'Start installer' or 'Start installer (expert mode)'.
+5. Continue through the installation until the 'Partition disks' step.
+6. Choose 'Manual' partitioning, and then create a single partition on the disk. Set up this partition as the root partition, and choose any normal root filesystem such as Ext4.
+7. Choose 'Finish partitioning and write changes to disk'. The installer will ask if you want to go back and set up swap space, choose 'No' when it asks if you want to return to the partitioning menu.
+8. Choose 'Yes' to write the changes to the disks on the next dialog.
+9. Open another SSH session to the installer. Choose 'Start shell', and then 'Continue'.
+10. Use nano to paste in the contents of the debian-encrypted-lvm-with-btrfs.sh script and save the file as run.sh. Run `chmod +x run.sh` to make the file executable.
+11. Run run.sh and follow the instructions that come up on screen.
---
+++ debian-encrypted-lvm-with-btrfs.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+
+# Set up constants.
+CRYPT_DM_NAME="cryptlvm"
+CRYPT_BLOCK_DEVICE="/dev/sda1"
+LVM_VG_NAME="vg"
+
+# Unmount existing target installer was going to install to.
+umount /target
+
+# Fail out of the script if any commands fail from this point forward.
+set -e
+
+# Install tools we need to set up the LUKS encrypted volume. (cryptsetup, etc.)
+anna-install cryptsetup-udeb cdebconf-newt-entropy partman-crypto-dm
+
+# Load dm_crypt, which will be needed to open the encrypted volume.
+depmod -a
+modprobe dm_crypt
+
+# Setup encrypted volume.
+cryptsetup --type luks1 -y -v luksFormat "$CRYPT_BLOCK_DEVICE"
+
+# Open encrypted volume.
+cryptsetup open "$CRYPT_BLOCK_DEVICE" "$CRYPT_DM_NAME"
+
+# Create LVM physical volume, volume group, and logical volumes.
+pvcreate "/dev/mapper/$CRYPT_DM_NAME"
+vgcreate "$LVM_VG_NAME" "/dev/mapper/$CRYPT_DM_NAME"
+lvcreate -L 8G "$LVM_VG_NAME" -n swap
+lvcreate -l 100%FREE "$LVM_VG_NAME" -n root
+
+# Setup Btrfs partition and swap.
+mkfs.btrfs "/dev/$LVM_VG_NAME/root"
+mkswap "/dev/$LVM_VG_NAME/swap"
+
+# Mount Btrfs partition and set up subvolumes.
+mount "/dev/$LVM_VG_NAME/root" /target
+cd /target
+btrfs subvol create @
+btrfs subvol create @home
+btrfs subvol create @snapshots
+btrfs subvol create @log
+
+cd /
+
+# Unmount Btrfs partition, and remount the subvolumes in the correct mount points.
+umount /target
+mount "/dev/$LVM_VG_NAME/root" /target -o subvol=@
+mkdir -p /target/home /target/var/log .btrfs
+mount "/dev/$LVM_VG_NAME/root" /target/home -o subvol=@home
+mount "/dev/$LVM_VG_NAME/root" /target/var/log -o subvol=@log
+
+read -p "Install the system packages and GRUB now. The GRUB install should fail. Return here after it fails. Press any key to resume ..."
+
+# Enable cryptodisk in GRUB, so GRUB install will work. 
+echo 'GRUB_ENABLE_CRYPTODISK=y' >> /target/etc/default/grub
+
+# Add cryptdevice, root, and rootflags to default kernel cmdline.
+sed -i "s,GRUB_CMDLINE_LINUX=\"\(.*\)\",GRUB_CMDLINE_LINUX=\"\1 cryptdevice=$CRYPT_BLOCK_DEVICE:$CRYPT_DM_NAME root=/dev/$LVM_VG_NAME/root rootflags=subvol=@\"," /target/etc/default/grub
+
+read -p "Repeat the 'Install GRUB' step. Then return to this script at the 'Finish the installation' step. Press any key to resume ..."
+
+mount --bind /dev /target/dev
+mount --bind /proc /target/proc
+mount --bind /sys /target/sys
+mount --bind /run /target/run
+
+chroot /target /bin/bash -c "
+apt install lvm2 cryptsetup snapper -y
+
+CRYPT_DM_NAME=\"$CRYPT_DM_NAME\"
+CRYPT_BLOCK_DEVICE=\"$CRYPT_BLOCK_DEVICE\"
+LVM_VG_NAME=\"$LVM_VG_NAME\"
+
+echo \"$CRYPT_DM_NAME  $CRYPT_BLOCK_DEVICE   /etc/keys/root.key    luks,key-slot=1\" >> /etc/crypttab
+echo \"/dev/mapper/$LVM_VG_NAME-root   /   btrfs   defaults,subvol=@   0   0\" >> /etc/fstab
+echo \"/dev/mapper/$LVM_VG_NAME-root   /.btrfs   btrfs   defaults   0   0\" >> /etc/fstab
+echo \"/dev/mapper/$LVM_VG_NAME-root   /.snapshots   btrfs   defaults,subvol=@snapshots   0   0\" >> /etc/fstab
+echo \"/dev/mapper/$LVM_VG_NAME-root   /home   btrfs   defaults,subvol=@home  0   0\" >> /etc/fstab
+echo \"/dev/mapper/$LVM_VG_NAME-root   /var/log   btrfs   defaults,subvol=@log   0   0\" >> /etc/fstab
+echo \"/dev/mapper/$LVM_VG_NAME-swap   none   swap   defaults   0   0\" >> /etc/fstab
+
+snapper --no-dbus -c root create-config /
+snapper --no-dbus -c home create-config /home
+snapper --no-dbus -c log create-config /var/log
+
+rm -rf /.snapshots
+mkdir .snapshots
+
+mount .snapshots
+
+chmod 750 .snapshots
+
+snapper --no-dbus -c root create --description initial
+snapper --no-dbus -c home create --description initial
+snapper --no-dbus -c log create --description initial"
+
+mkdir -m0700 /target/etc/keys
+( umask 0077 && dd if=/dev/urandom bs=1 count=64 of=/target/etc/keys/root.key conv=fsync )
+cryptsetup luksAddKey $CRYPT_BLOCK_DEVICE /target/etc/keys/root.key
+echo "KEYFILE_PATTERN=\"/etc/keys/*.key\"" >> /target/etc/cryptsetup-initramfs/conf-hook
+echo UMASK=0077 >> /target/etc/initramfs-tools/initramfs.conf
+
+echo 'Finish the installation and reboot.'
